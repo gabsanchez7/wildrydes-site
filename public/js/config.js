2d5ec60609c8c31bd9cdebf66d020cc6cb3d21b8
Gabe <gabrielsanchez@my.unt.edu>
2022-12-04 18:55:19 -0600

    Changing Layout

--- public/js/config.js
+++ public/js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId:         '',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region:             ''          // e.g. us-east-2
+        userPoolId:         'us-west-2_2ME5r1bDM',         // e.g. us-east-2_uXboG5pAb
+        userPoolClientId:   '3u2l630qeh47r95le0bmqbo5no',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region:             'us-east-2'          // e.g. us-east-2
     },
     api: {
         invokeUrl:          ''          // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
